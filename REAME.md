566358c0b1eb649a2cfafd5a65ce92bd367caa82
calnix <calnix.289@gmail.com>
2022-07-27 17:57:27 +0800

    initial code

---
+++ REAME.md
@@ -0,0 +1,5 @@
+TODO:
+
+- how tf arweave?
+- upgradeable proxy patterns - ERC1967 or Diamond (OZ)
+- module extensions
--- src/Contract.sol
+++ src/Contract.sol
@@ -1,4 +1,30 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity 0.8.13;
 
-contract Contract {}
+import {Counters} from "lib/openzeppelin-contracts/contracts/utils/Counters.sol";
+
+contract Wiki {
+    using Counters for Counters.Counter;
+
+    Counters.Counter public id;
+    Counters.Counter public index;
+
+    mapping (uint256 id => mapping(uint256 index => string link));
+    mapping (uint256 id => uint256 numEntries);
+
+    event Created( address indexed from, uint256 indexed id, uint256 indexed index, string article);
+
+    event Updated( address indexed from, uint256 indexed id, uint256 indexed index, string article);
+
+    function create(string calldata link) public {
+        //require();
+
+
+    }
+
+    function read() public view {}
+
+    function update() public {}
+
+    //function moduleCall() {} // extensibility
+}
