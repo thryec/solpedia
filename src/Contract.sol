636df65fce0a5727d0f353d5b441a09afc53ade1
calnix <calnix.289@gmail.com>
2022-08-31 20:30:03 +0800

    Bali commit

--- src/Contract.sol
+++ src/Contract.sol
@@ -6,25 +6,54 @@ import {Counters} from "lib/openzeppelin-contracts/contracts/utils/Counters.sol"
 contract Wiki {
     using Counters for Counters.Counter;
 
-    Counters.Counter public id;
-    Counters.Counter public index;
+    Counters.Counter public articleId;
+    Counters.Counter public versionId;
 
-    mapping (uint256 id => mapping(uint256 index => string link));
-    mapping (uint256 id => uint256 numEntries);
+    //article struct
+    struct article {
+        uint256 articleId
+        uint256 versionId
+        //address lastEditor 
+    }
+    
+    // articles/hashes in universe
+    mapping (string ipfsHash => boolean) public articles;
 
-    event Created( address indexed from, uint256 indexed id, uint256 indexed index, string article);
+    mapping (uint256 articleId => mapping(uint256 versionId => string ipfsHash)) public versionTree;
 
-    event Updated( address indexed from, uint256 indexed id, uint256 indexed index, string article);
+    //map user actions to wallet addresses - who did what
+    mapping (uint256 articleId => address user) public authors;
+    mapping (uint256 versionId => address user) public editors;
 
-    function create(string calldata link) public {
-        //require();
 
+    event Created( address indexed from, uint256 indexed articleId, uint256 indexed versionId, string ipfsHash);
+    event Updated( address indexed from, uint256 indexed articleId, uint256 indexed versionId, string ipfsHash);
+
+    function create(string calldata ipfsHash) external {
+        // require(ipfsHash should be unique);
+        
+        require(articles(ipfsHash) == false);         // article hash should not exist
+        
+        uint256 newArticleId = articleId.current();   // starts frm 0
+        uint256 newVersionId = versionId.current();
+
+        versionTree(newArticleId)() = 
 
     }
 
-    function read() public view {}
+    function update() public {
+        require(articles(ipfsHash) == true); //article hash should exist
 
-    function update() public {}
+    }
 
     //function moduleCall() {} // extensibility
 }
+
+
+// id -> each article has a unique ID
+// versionIndex -> version iteration
+//
+// Actions -> {create,modify}
+
+// https://docs.openzeppelin.com/contracts/4.x/utilities#base64
+// for ipfs hash handling
