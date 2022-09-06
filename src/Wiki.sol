5b28261829ffea28c15390f7f0a0258da32f2dda
Chloe Tan <thryec@gmail.com>
2022-09-06 15:23:16 +0800

    added natspec comments

--- src/Wiki.sol
+++ src/Wiki.sol
@@ -8,19 +8,29 @@ import {console2} from "forge-std/console2.sol";
 contract Wiki {
     using Counters for Counters.Counter;
 
+    /// @notice Counter to keep track of unique article IDs.
     Counters.Counter public articleId;
+
+    /// @notice Counter to keep track of unique version IDs for each article.
     Counters.Counter public versionId;
 
+    /// @notice Mapping to match specific article and version IDs to their editors.
     mapping(string => address) editors;
+
+    /// @notice Mapping to match specific article and version IDs to their IPFS CIDs.
     mapping(string => string) links;
+
+    /// @notice Mapping to track the number of versions in each article.
     mapping(uint256 => uint256) versions;
 
+    /// @notice Emits when a new article is created.
     event ArticleCreated(
         uint256 indexed articleId,
         address indexed creator,
         string ipfsHash
     );
 
+    /// @notice Emits when a new version is created for an article.
     event NewVersionCreated(
         uint256 indexed articleId,
         uint256 indexed versionId,
@@ -28,6 +38,11 @@ contract Wiki {
         string ipfsHash
     );
 
+    /**
+     * @notice Creates a new article.
+     * @param ipfsHash The unique IPFS CID referencing the contents of the article
+     * @return createdArticleId The article ID of the newly created article.
+     */
     function createArticle(string calldata ipfsHash)
         public
         returns (uint256 createdArticleId)
@@ -53,6 +68,13 @@ contract Wiki {
         emit ArticleCreated(currentArticleId, msg.sender, ipfsHash);
         return currentArticleId;
     }
+
+    /**
+     * @notice Creates a new version of an existing article.
+     * @dev Throws if the article referenced in params has not been created yet.
+     * @param existingArticleId The ID of the article that a new version will be added to.
+     * @param ipfsHash The unique IPFS CID referencing the contents of the article
+     */
 
     function addVersion(uint256 existingArticleId, string calldata ipfsHash)
         public
--- test/Wiki.sol
+++ test/Wiki.sol
@@ -6,18 +6,34 @@ import "forge-std/Test.sol";
 import {Vm} from "forge-std/Vm.sol";
 import {console2} from "forge-std/console2.sol";
 
-contract ContractTest is Test {
+// contract ContractTest is Test {
+//     Wiki internal wiki;
+
+//     function setUp() public {
+//         wiki = new Wiki();
+//     }
+
+//     function testCreateArticle() public {
+//         uint256 articleId = wiki.createArticle(ipfsHash1);
+//         console2.log("new article id", articleId);
+//         wiki.addVersion(articleId, ipfsHash1);
+//     }
+// }
+
+abstract contract StateZero is Test {
     Wiki internal wiki;
 
-    string ipfsHash1 = "bafybohew2j2wbn3mzl7dakkoklstoas4jq3rj7wgiv6mmtvk7v7a";
-
-    function setUp() public {
+    function setUp() public virtual {
         wiki = new Wiki();
     }
+}
+
+contract StateZeroTest is StateZero {
+    string ipfsHash1 = "bafybohew2j2wbn3mzl7dakkoklstoas4jq3rj7wgiv6mmtvk7v7a";
 
     function testCreateArticle() public {
         uint256 articleId = wiki.createArticle(ipfsHash1);
         console2.log("new article id", articleId);
-        wiki.addVersion(articleId, ipfsHash1);
+        assertEq(articleId, 0);
     }
 }
