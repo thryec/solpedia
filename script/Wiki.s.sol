d02225a0c501085c6a2035786e6377413c1752fc
Chloe Tan <thryec@gmail.com>
2022-09-15 18:07:27 +0800

    deployed proxy in script

--- script/Wiki.s.sol
+++ script/Wiki.s.sol
@@ -3,6 +3,8 @@ pragma solidity 0.8.16;
 
 import "forge-std/Script.sol";
 import "../src/Wiki.sol";
+import "../src/WikiProxy.sol";
+import "forge-std/console2.sol";
 
 contract WikiScript is Script {
     function setUp() public {}
@@ -11,7 +13,11 @@ contract WikiScript is Script {
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
 
-        new Wiki();
+        Wiki wiki = new Wiki();
+        WikiProxy proxy = new WikiProxy();
+
+        console2.log("wiki address", address(wiki));
+        console2.log("proxy address", address(proxy));
 
         vm.stopBroadcast();
     }
